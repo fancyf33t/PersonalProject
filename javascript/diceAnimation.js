9e4dd940eb34b4bf34aa1b68c1eb360542e7b2d8
fancyf33t <fancyf33t@protonmail.com>
2021-10-23 19:28:13 -0500

    die works. kind of

--- javascript/diceAnimation.js
+++ javascript/diceAnimation.js
@@ -1,9 +1,40 @@
 // I want to make a dice do simple animation
-myVariable=setInterval(myfunction, time interval)
 
-ranNum = Math.floor( 1 + Math.random() * 6 );
+// What exactly do I need to do to make that happen?
 
-var ranNum = Math.floor( 1 + Math.random() * 6 );
-var dice = document.getElementById("die");
-dice.src=ranNum+".jpg";
+// Get random number intervals?
+
+ranNum = Math.floor( 1 + Math.random() * 6 ); // ranNum is the variable storing Math.floor?
+
+var dice = document.getElementsByClassName("die");
+dice.src=ranNum+".png";
+
+function AniDice()
+{
+myVar=setInterval(rolldice,20)
 }
+
+function rolldice()
+{
+var ranNum = Math.floor( 1 + Math.random() * 6 );
+var dice = document.getElementByClassName("die");
+dice.src=ranNum+".png";
+}
+function stopDice()
+{clearInterval(myVar);}
+
+// this is digital
+
+function AniDice()
+{
+MyVar=setInterval(rolldice,20)
+}
+
+function rolldice()
+{
+var ranNum = Math.floor( 1 + Math.random() * 6 );
+document.getElementById("dice").innerHTML = ranNum;
+
+}
+function stopDice()
+{clearInterval(MyVar);}
