c4a9caca30c60221bb11426027c5d74e4d755eab
fancyf33t <fancyf33t@protonmail.com>
2021-10-30 21:32:00 -0500

    working on new sheets

---
+++ javascript/characterSheetArrays.js
@@ -0,0 +1,26 @@
+// let's start with getting a blank array set up for the character
+
+
+
+var Jim_Culver;
+
+Jim_Culver = [
+    ['Health', 5],
+    ['Sanity', 7],
+    ['Lore', 3],
+    ['Influence', 3],
+    ['Observation', 2],
+    ['Strength', 2],
+    ['Will', 3],
+    ['Condition',
+        ['Conditions Inventory',]],
+    ['Items',
+        ['Items Inventory', 
+            ['Clue', 1]]],
+    ['Spells',
+        ['Spell Inventory', 'Shriveling Spell']],
+];
+
+console.log(Jim_Culver)
+
+// now i need to create a card that will affect my character sheet... duck...
