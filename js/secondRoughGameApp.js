eb3ef17bdf6ede2dff1fa0bf52e24826ae4dcd6b
fancyf33t <fancyf33t@protonmail.com>
2022-01-11 09:55:57 -0600

    character load works

--- js/secondRoughGameApp.js
+++ js/secondRoughGameApp.js
@@ -2,12 +2,174 @@
 
 class Game {
     // for some reason, getting rid of the constructor fixed it?? i don't know why
+    // 1/11 i will attempt to reintroduce the constructor. that may help things
+    constructor() {
+        this.characters = {
+            character01: {
+                name: 'Diana Stanley',
+                id: 'The Redeemed Cultist',
+                img: 'media/dianastanley.jpg',
+                health: 7,
+                sanity: 5,
+                lore: 4,
+                influence: 2,
+                observation: 3,
+                strength: 3,
+                will: 1,
+                // add items later
+            },
+            character02: {
+                name: 'Trish Scarborough',
+                id: 'The Spy',
+                img: 'media/trishscarborough.jpg',
+                health: 7,
+                sanity: 5,
+                lore: 1,
+                influence: 3,
+                observation: 4,
+                strength: 3,
+                will: 2
+                // add items later
+            },
+            character03: {
+                name: 'Norman Withers',
+                id: 'The Astronomer',
+                img: 'media/normanwithers.jpg',
+                health: 5,
+                sanity: 7,
+                lore: 3,
+                influence: 1,
+                observation: 3,
+                strength: 2,
+                will: 4,
+                // add items later
+            },
+            character04: {
+                name: 'Lola Hayes',
+                id: 'The Actress',
+                img: 'media/lolahayes.jpg',
+                health: 5,
+                sanity: 7,
+                lore: 2,
+                influence: 4,
+                observation: 2,
+                strength: 2,
+                will: 3,
+                // add items later
+            },
+            character05: {
+                name: 'Jim Culver',
+                id: 'The Musician',
+                img: 'media/jimculver.jpg',
+                health: 7,
+                sanity: 5,
+                lore: 3,
+                influence: 2,
+                observation: 2,
+                strength: 2,
+                will: 3,
+                // add items later
+            },
+            character06: {
+                name: 'Charlie Kane',
+                id: 'The Politician',
+                img: 'media/charliekane.jpg',
+                health: 4,
+                sanity: 8,
+                lore: 2,
+                influence: 4,
+                observation: 3,
+                strength: 2,
+                will: 2,
+                // add items later
+            },
+            character07: {
+                name: 'Lily Chen',
+                id: 'The Martial Artist',
+                img: 'media/lilychen.jpg',
+                health: 6,
+                sanity: 6,
+                lore: 2,
+                influence: 2,
+                observation: 2,
+                strength: 4,
+                will: 3,
+                // add items later
+            },
+            character08: {
+                name: 'Akachi Onyele',
+                id: 'The Shaman',
+                img: 'media/akachionyele.jpg',
+                health: 5,
+                sanity: 7,
+                lore: 3,
+                influence: 2,
+                observation: 2,
+                strength: 2,
+                will: 4,
+                // add items later
+            },
+            character09: {
+                name: 'Silas Marsh',
+                id: 'The Sailor',
+                img: 'media/silasmarsh.jpg',
+                health: 8,
+                sanity: 4,
+                lore: 1,
+                influence: 3,
+                observation: 3,
+                strength: 3,
+                will: 3,
+                // add items later
+            },
+            character10: {
+                name: 'Jacqueline Fine',
+                id: 'The Psychic',
+                img: 'media/jacquelinefine.jpg',
+                health: 4,
+                sanity: 8,
+                lore: 4,
+                influence: 2,
+                observation: 3,
+                strength: 1,
+                will: 3,
+                // add items later
+            },
+            character11: {
+                name: 'Leo Anderson',
+                id: 'The Expedition Leader',
+                img: 'media/leoanderson.jpg',
+                health: 6,
+                sanity: 6,
+                lore: 2,
+                influence: 2,
+                observation: 3,
+                strength: 3,
+                will: 3,
+                // add items later
+            },
+            character12: {
+                name: 'Mark Harrigan',
+                id: 'The Soldier',
+                img: 'media/markharrigan.jpg',
+                health: 8,
+                sanity: 4,
+                lore: 1,
+                influence: 2,
+                observation: 2,
+                strength: 4,
+                will: 4,
+                // add items later
+            },
+        }
+    }
     init() {
         // leave your functions here...
         this.removeDie();
         this.rollDie();
         this.resetDie();
         this.displayEncounter();
+        this.characterLoad();
 
     }
     // this function allows the die to roll and appear in the boxes
@@ -103,6 +265,66 @@ class Game {
      * maybe that will be the task for 1/11
      * read through modular folder
      */
+    characterLoad() {
+        // load characters on page
+        let characterCount = 0;
+
+        // access HTML nodes
+        let output = document.getElementById('characterOutput');
+        let row1 = document.getElementById('row1'),
+            row2 = document.getElementById('row2'),
+            row3 = document.getElementById('row3'),
+            row4 = document.getElementById('row4'),
+            row5 = document.getElementById('row5'),
+            row6 = document.getElementById('row6');
+
+        for (const key in this.characters) {
+            const character = this.characters[key]
+            const hero = document.createElement('div');
+            hero.className = 'col-md-4 hero';
+            hero.innerHTML = `
+            <div class="container">
+            <div class="row no-gutters">
+            <div class="col-md-4">
+                <img src="${character.img}" alt="${character.name}" class="img-fluid character-img">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${character.name}</h5>
+                    <p class="card-text"><em>"${character.id}"</em></p>
+                    <ul class="nav card-stats">
+                        <li class="stats-box" id="stats1"><span id="lore">${character.lore}</span></li>
+                        <li class="stats-box" id="stats2"><span id="influence">${character.influence}</span></li>
+                        <li class="stats-box" id="stats3"><span id="observation">${character.observation}</span></li>
+                        <li class="stats-box" id="stats4"><span id="strength">${character.strength}</span></li>
+                        <li class="stats-box" id="stats5"><span id="will">${character.will}</span></li>
+                    </ul>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="health-box">${character.health}</div>
+                            </div>
+                            <div class="col-4">
+                                <div class="sanity-box">${character.sanity}</div>
+                            </div>
+                            <div class="col-4">
+                                <div class="clues-box"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+
+            // 1/10 holy crap it works.. freaking finally
+            // can i nest the if statements?
+            if (characterCount < 3) {
+                row1.append(hero)
+            } else {
+                row2.append(hero);
+            }
+            characterCount++;
+        }
+    }
 
 }
 
@@ -112,4 +334,6 @@ action.init()
 // this section will be outside of init() for now.
 
 // 1/10/22 ok. so everything is inside the init(). you are close to being a big boy...
-// 1/11/22 So, I want to use import.js for this portion to show personal growth. How do I do that?
+// 1/11/22 So, I want to use import.js for this portion to show personal growth. How do I do that? Scratch that idea. NO IMPORTING
+
+
