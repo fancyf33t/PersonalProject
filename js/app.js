9efb2876f53ebc60955910915ed7347d8b64e426
fancyf33t <fancyf33t@protonmail.com>
2022-01-13 13:16:59 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -186,11 +186,102 @@ class Game {
             // figure it out
             encounter10: 'A shady figure offers to sell you a weapon, no questions asked. You speak with him to detrmine his motives [influence]. If you pass, gain 1 random Weapon Asset from the deck. If you fail, the undercover policeman arrests you; gain a Detained Condition.',
             // figure it out
+            encounter11: 'You help a young woman search the countryside for her uncle [observation]. If you pass, you find the eccentric, old man, and he gives you a gift for helping his niece; gain 1 Tome Artifact from the deck',
+            // test observation, if success += artifact
+            encounter11: 'In the middle of the night, a feral beast rips apart your camp and attacks you [strength]! If you pass, you feel invincible; improve [will]. If you fail, lose 1 Health and gain a Leg Injury Condition.',
+            // test strength; if success +1 will; if fail, -1 health && +injury
+            encounter12: 'Far from any road or village, you find a shallow grave marked only with an arcane symbol. As you dig, a growing dread weakens your resolve [will]. If you pass, you find a charred corpse clutching a journal; gain 1 Clue or improve [lore]. If you fail, you run from the area; gain a Cursed Condition.',
+            // test will; if succes +1 clue || +1 lore
+            encounter13: 'According to your map, you can make a detour to visit a lake purported to have restorative powers. You may become Delayed to recover 3 Sanity or discard a Madness Condition.',
+            // later
+            encounter14: 'You scrape away years of moss and lichen from the stone altar, uncovering a series of prehistoric symbols. You think you can interpret the carvings [lore]. If you pass, gain 1 Clue or 1 Spell.',
+            // am i even doing spells?
+            encounter15: 'Extreme conditions have left you fevered and delirious. You force yourself to keep moving, putting a terrible strain on your body [strength]. If you fail, the fever grows worse and you begin seeing things; gain a Hallucinations Condition.',
+            //
+            encounter16: 'You find a primitive painting on the wall of a cave and decide to explore the subterranean depths that lie beyond. The dark, constricting passages create a terrible sense of claustrophobia [-1 will]. If you pass, gain 1 Artifact left by an ancient civilization. If you fail, lose 2 Sanity.',
+            // test will -1; if success, +1 artifact; if fail, -2 sanity
+            encounter17: 'The terrain ahead of you looks impassable, but going around would take too long. You\'ll need to find some way to move forward [observation]. If you pass, you discover a path; move 1 space. If you fail, you trip over the uneven ground; lose 1 Health and gain a Leg Injury Condition.',
+            encounter18: 'Your map is gone, and you must navigate based on your own observations [observations]. If you pass, you confidently progress on your journey; move 1 space. If you fail, you travel in circles; you become Delayed and gain a Madness Condition.',
+            // test observation
+            encounter19: 'The ground slopes upward so uniformly that you suspect a structure exists unerneath. You search for a buried entrance to claim the treasures found inside. You may spend 1 Clue to gain 1 Artifact.',
+            encounter20: 'Past where your campfire allows you to see, you hear voices chanting. You try to draw a protective sigil in the ground [lore]. If yo pass, the next morning you find evidence of cult activity that you can use to identify the cult members; gain 1 Clue or improve [influence]. If you fail, gain a Cursed Condition.',
+            encounter21: 'The ship ahead of you seemed abandoned, but now you see that it is crewed entirely by ghostly figures. You try to discern what ship this had once been and what happened to it [lore]. If you pass, the spectral captain grants you aid; gain 1 Clue or improve [will]. If you fail, gain a Cursed Condition.',
+            encounter22: 'The captain of the ship invites you to dine with him. You have the feeling that he\'s had some experience with unearthly creatures and try to convince him to share his stories [influence]. If you pass, his tale includes highly-significant details; spawn 1 Clue.',
+            encounter23: 'Your ship becomes lost in a dense fog bank, terrifying the superstitious crew. When the mist finally clears, you\'ve somehow traveled hundreds of miles. Move 1 space and lose 1 Sanity.',
+            encounter24: 'One of the sailors speaks in a strange, ancient dialect. You try to communicate with the man based on obscure languages you have studied [lore]. If you pass, the peculiar man teaches you a chant; gain 1 Spell. If you fail, he growls an unintelligible phrase; gain a Cursed Condition.',
+            encounter25: 'One of the sailors is singing an old sea shanty about a woman searching for her dead husband. The story seems familiar to you [-1 lore]. If you pass, you recognize it as the story of Isis, and the sailor teaches you the song; gain a Blessed Condition and recover 1 Health and 1 Sanity.',
+            encounter26: 'You\'re awakened in the night by gunfire. Deep ones have climbed aboard the ship and are trying to sabotage the engine. You do your best to help the crew fight them [strength]. If you fail, the engine is destroyed before you finish off the sea creatures, and you must wait to be rescued; become Delayed.',
+            encounter27: 'A large wave washes across the deck, and a prized possession slips from your fingers. You dive into the water, holding your breath as long as you can to recover the object before it sinks out of reach [will]. If you fail, discard 1 Item possession.',
+            encounter28: 'You discover a signal fire on a small island, but don\'t find any people. You search the beach for signs of life [observation]. If you pass, you spot a person hiding behind large stones; gain 1 Clue and 1 random Ally Asset from the deck. If you fail, the mystery remains unsolved; gain a Paranoia Condition.',
+            encounter29: 'A sudden storm descends upon you and strong winds whip around your vessel. Huge waves toss your ship around like a toy, and you are thrown to the deck repeatedly. Lose 1 Sanity and gain a Back Injury Condition.',
+            encounter30: 'The captain tells you that you are passing over the site of a famous shipwreck. You can use the ship\'s deep-sea diving equipment to explore the wreckage. You may become Delayed to gain 1 Artifact.'
+        }
+        this.monsters = {
+            monster01: {
+                name: 'Cthulhu',
+                health: 10,
+                sanity: 8,
+                media: 'media/Monsters/cthulhu.jpg',
+            },
+            monster02: {
+                name: 'Cultist',
+                health: 3,
+                sanity: 2,
+                media: 'media/Monsters/monster10.jpg',
+            },
+            monster03: {
+                name: 'Wriggler',
+                health: 2,
+                sanity: 4,
+                media: 'media/Monsters/monster1.jpg'
+            },
+            monster04: {
+                name: 'Many Beasts',
+                health: 3,
+                sanity: 1,
+                media: 'media/Monsters/monster2.jpg'
+            },
+            monster05: {
+                name: 'Shambler',
+                health: 2,
+                sanity: 2,
+                media: 'media/Monsters/monster3.jpg'
+            },
+            monster06: {
+                name: 'Ancient One',
+                health: 5,
+                sanity: 5,
+                media: 'media/Monsters/monster4.jpg'
+            },
+            monster07: {
+                name: 'Deep One',
+                health: 2,
+                sanity: 1,
+                media: 'media/Monsters/monster5.jpg'
+            },
+            monster08: {
+                name: 'Nyerlatothep',
+                health: 4,
+                sanity: 4,
+                media: 'media/Monsters/monster6.jpg'
+            },
+            monster09: {
+                name: 'Black Sheep',
+                health: 3,
+                sanity: 3,
+                media: 'media/Monsters/monster9.jpg'
+            },
+            monster01: {
+                name: 'Shub Niggurath',
+                health: 7,
+                sanity: 7,
+                media: 'media/Monsters/monster7.jpg'
+            }
         }
     }
     init() {
         // leave your functions here...
-        
+
         this.removeDie();
         this.rollDie();
         this.resetDie();
@@ -264,11 +355,10 @@ class Game {
                 e.preventDefault();
                 console.log('you clicked the box')
                 // the problem isn't making the dice disappear, it is making sure that I am not having the dice run in the background
-                die.style.display = 'none'
+                // die.style.display = 'none';
+                die.innerText = 0;
             })
         })
-
-
     }
     // display random encounters?
     // 1/8 make sure that you figure out how to call on MULTIPLE encounters that are saved
@@ -293,13 +383,53 @@ class Game {
                 encounter04: 'Legends say that this cemetery is haunted. Exploring the headstones, you encounter an angry specter, eager to share his story. The experience is nerve-wracking, but you try to listen [will]. If you pass, the spirit gratefully fades from view; recover 2 Sanity. If you fail, his desperate voice echoes in your mind; gain a Paranoia Condition.',
                 // test will; if success, sanity + 2 else condition[paranoia
                 encounter05: 'The shop is robbed while you are browsing! You attempt to fend off the thieves [strength]. If you pass, the store owner is very gracious; gain 1 Item Asset from the reserve or 1 random Item Asset from the deck. If you fail, lose 1 Health and discard 1 Item possession.',
+                encounter06: 'The police ask for your help investigating a series of ritual killings. The grisly crime scenes threaten to overwhelm you with chills and nausea [will]. If you pass, you manage to examine the scene and find significant information; spawn 1 Clue. If you fail, you can\'t endure the horror; lose 2 Sanity.',
+                // test will; if success, clue + 1 else sanity - 2
+                encounter07: 'The Syndicate is rengaged in a gang war with local thugs and is under intense legal scrutiny. You try to trade your political clout for assistance [influence]. If you pass, gain 1 Service Asset from the reserve or 1 random Service Asset from the deck.',
+                // test influence; if success inventory + asset.service
+                encounter08: 'A group of intimidating Syndicate members demand you pay them for protection. You offer the well-dressed men what you can afford [influence]. If you fail, they make sure you meet with an accident; gain a Leg Injury Condition.',
+                // figure it out
+                encounter09: 'A friendly game of cards ends with a very high-stakes hand [influence]. If you pass, you amaze everyone watching and find a new friend; gain 1 random Ally Asset from the deck. If you fail, gain a Debt Condition to cover the loss.',
+                // figure it out
+                encounter10: 'A shady figure offers to sell you a weapon, no questions asked. You speak with him to detrmine his motives [influence]. If you pass, gain 1 random Weapon Asset from the deck. If you fail, the undercover policeman arrests you; gain a Detained Condition.',
+                // figure it out
+                encounter11: 'You help a young woman search the countryside for her uncle [observation]. If you pass, you find the eccentric, old man, and he gives you a gift for helping his niece; gain 1 Tome Artifact from the deck',
+                // test observation, if success += artifact
+                encounter11: 'In the middle of the night, a feral beast rips apart your camp and attacks you [strength]! If you pass, you feel invincible; improve [will]. If you fail, lose 1 Health and gain a Leg Injury Condition.',
+                // test strength; if success +1 will; if fail, -1 health && +injury
+                encounter12: 'Far from any road or village, you find a shallow grave marked only with an arcane symbol. As you dig, a growing dread weakens your resolve [will]. If you pass, you find a charred corpse clutching a journal; gain 1 Clue or improve [lore]. If you fail, you run from the area; gain a Cursed Condition.',
+                // test will; if succes +1 clue || +1 lore
+                encounter13: 'According to your map, you can make a detour to visit a lake purported to have restorative powers. You may become Delayed to recover 3 Sanity or discard a Madness Condition.',
+                // later
+                encounter14: 'You scrape away years of moss and lichen from the stone altar, uncovering a series of prehistoric symbols. You think you can interpret the carvings [lore]. If you pass, gain 1 Clue or 1 Spell.',
+                // am i even doing spells?
+                encounter15: 'Extreme conditions have left you fevered and delirious. You force yourself to keep moving, putting a terrible strain on your body [strength]. If you fail, the fever grows worse and you begin seeing things; gain a Hallucinations Condition.',
+                //
+                encounter16: 'You find a primitive painting on the wall of a cave and decide to explore the subterranean depths that lie beyond. The dark, constricting passages create a terrible sense of claustrophobia [-1 will]. If you pass, gain 1 Artifact left by an ancient civilization. If you fail, lose 2 Sanity.',
+                // test will -1; if success, +1 artifact; if fail, -2 sanity
+                encounter17: 'The terrain ahead of you looks impassable, but going around would take too long. You\'ll need to find some way to move forward [observation]. If you pass, you discover a path; move 1 space. If you fail, you trip over the uneven ground; lose 1 Health and gain a Leg Injury Condition.',
+                encounter18: 'Your map is gone, and you must navigate based on your own observations [observations]. If you pass, you confidently progress on your journey; move 1 space. If you fail, you travel in circles; you become Delayed and gain a Madness Condition.',
+                // test observation
+                encounter19: 'The ground slopes upward so uniformly that you suspect a structure exists unerneath. You search for a buried entrance to claim the treasures found inside. You may spend 1 Clue to gain 1 Artifact.',
+                encounter20: 'Past where your campfire allows you to see, you hear voices chanting. You try to draw a protective sigil in the ground [lore]. If yo pass, the next morning you find evidence of cult activity that you can use to identify the cult members; gain 1 Clue or improve [influence]. If you fail, gain a Cursed Condition.',
+                encounter21: 'The ship ahead of you seemed abandoned, but now you see that it is crewed entirely by ghostly figures. You try to discern what ship this had once been and what happened to it [lore]. If you pass, the spectral captain grants you aid; gain 1 Clue or improve [will]. If you fail, gain a Cursed Condition.',
+                encounter22: 'The captain of the ship invites you to dine with him. You have the feeling that he\'s had some experience with unearthly creatures and try to convince him to share his stories [influence]. If you pass, his tale includes highly-significant details; spawn 1 Clue.',
+                encounter23: 'Your ship becomes lost in a dense fog bank, terrifying the superstitious crew. When the mist finally clears, you\'ve somehow traveled hundreds of miles. Move 1 space and lose 1 Sanity.',
+                encounter24: 'One of the sailors speaks in a strange, ancient dialect. You try to communicate with the man based on obscure languages you have studied [lore]. If you pass, the peculiar man teaches you a chant; gain 1 Spell. If you fail, he growls an unintelligible phrase; gain a Cursed Condition.',
+                encounter25: 'One of the sailors is singing an old sea shanty about a woman searching for her dead husband. The story seems familiar to you [-1 lore]. If you pass, you recognize it as the story of Isis, and the sailor teaches you the song; gain a Blessed Condition and recover 1 Health and 1 Sanity.',
+                encounter26: 'You\'re awakened in the night by gunfire. Deep ones have climbed aboard the ship and are trying to sabotage the engine. You do your best to help the crew fight them [strength]. If you fail, the engine is destroyed before you finish off the sea creatures, and you must wait to be rescued; become Delayed.',
+                encounter27: 'A large wave washes across the deck, and a prized possession slips from your fingers. You dive into the water, holding your breath as long as you can to recover the object before it sinks out of reach [will]. If you fail, discard 1 Item possession.',
+                encounter28: 'You discover a signal fire on a small island, but don\'t find any people. You search the beach for signs of life [observation]. If you pass, you spot a person hiding behind large stones; gain 1 Clue and 1 random Ally Asset from the deck. If you fail, the mystery remains unsolved; gain a Paranoia Condition.',
+                encounter29: 'A sudden storm descends upon you and strong winds whip around your vessel. Huge waves toss your ship around like a toy, and you are thrown to the deck repeatedly. Lose 1 Sanity and gain a Back Injury Condition.',
+                encounter30: 'The captain tells you that you are passing over the site of a famous shipwreck. You can use the ship\'s deep-sea diving equipment to explore the wreckage. You may become Delayed to gain 1 Artifact.'
             }
-            sceneChange = encounters[Object.keys(encounters)[Math.floor(Math.random()*Object.keys(encounters).length)]];
-            
-            console.log(encounters[Object.keys(encounters)[Math.floor(Math.random()*Object.keys(encounters).length)]]);
+            // randomize the events
+            sceneChange = encounters[Object.keys(encounters)[Math.floor(Math.random() * Object.keys(encounters).length)]];
+
+            console.log(encounters[Object.keys(encounters)[Math.floor(Math.random() * Object.keys(encounters).length)]]);
             encounterDisplay.innerHTML = sceneChange;
             // it is starting to work in my favor; now i need to include span id
-            if (sceneChange.includes("[strength]")){
+            if (sceneChange.includes("[strength]")) {
                 console.log('This text contains the word strength')
                 // stats-box.style.borderColor = 'red';
             };
@@ -422,7 +552,15 @@ class Game {
         // do a forEach => forEach item in hero addEventListener
         // console.log('Are you working') // yes you are working
     }
+    // display monster&stats
+    displayMonster() {
+        // i want the information o the monsters to display here
+        let monsterDisplay = document.getElementById('monsterImage'),
+            statDisplay = document.getElementById('statDisplay');
+        let randomEncounter = document.getElementById('changeSceneBtn'),
 
+        
+    }
     // create a turn counter?
 }
 
